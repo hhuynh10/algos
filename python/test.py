ed4b315a080ab203b0f80e9a89fbee30546da261
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2023-01-16 23:25:19 -0600

    algos

---
+++ python/test.py
@@ -0,0 +1,45 @@
+string = "I hate you"
+
+strArray = string.split(" ")
+
+print(strArray)
+
+string1 = " ".join(strArray)
+
+print(string1)
+
+print(string.find("hate"))
+print(string.endswith("e"))
+print(string.isalpha())
+
+
+strArray1 = strArray[:]
+
+print(strArray1.index('hate'))
+
+for i in range(1, 21):
+    print(f"Number: {i}")
+
+
+count = 0
+while count <= 5:
+    print("looping - ", count)
+    count += 1
+
+my_list = ["abc", 123, "xyz"]
+for i in range(0, len(my_list)):
+    print(i, my_list[i])
+
+for v in my_list:
+    print(v)
+
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
